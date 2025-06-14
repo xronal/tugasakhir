42856fffab07967116bbdd88f0e9103a369667d7
xronal <driverronal@gmail.com>
2025-06-14 15:30:46 +0700

    Error ketika migrate dan keluar undefined property: stdClass::

---
+++ database/seeders/CampsiteSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Campsite;
+
+class CampsiteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $datas = json_decode(file_get_contents('database/seeders/json/campsites.json'));
+
+        DB::beginTransaction();
+        try {
+            foreach ($datas as $key => $data) {
+                $campsite = new Campsite();
+                $campsite->campsite_code = $data->campsites_code;
+                $campsite->campsite_name = $data->campsites_name;
+                $campsite->weekday_price = $data->weekday_price;
+                $campsite->weekend_price = $data->weekend_price;
+                $campsite->description = $data->description;
+                $campsite->save();
+                DB::commit();
+            }
+        } catch (\Exception $ex) {
+            echo $ex->getMessage();
+            DB::rollBack();
+        }
+    }
+}
